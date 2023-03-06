0ff8d7773f1c26b55575b1d68459df9dc3eb83f5
szwaymi <61036584+szwaymi@users.noreply.github.com>
2023-03-06 11:06:53 +0800

    [22] Trying Solution A.

--- 22/main.c
+++ 22/main.c
@@ -3,93 +3,77 @@
 #include <stdio.h>
 #include <string.h>
 
-struct sParenthese{
-    unsigned int iNumber;
-    struct sParenthese *pmChildren;
-};
-struct sResult{
-    struct sResult *pmNext;
-    char *pcParentheses;
+struct sCache {
+	unsigned int iCombinations;
+	char **ppcParentheses;
 };
 
-struct{
-    unsigned int iResults;
-    unsigned int iNumber;
-    struct sParenthese mParentheses;
-    struct sResult *pmResults;
-    char *pcParenceses;
+struct {
+	struct sCache *pmCaches;
 }gmMeta;
 
-void rTrace(struct sParenthese *pmParent){
-    unsigned int iCon;
-    for(iCon = 0 ; iCon < pmParent->iNumber ; iCon++){
-        *gmMeta.pcParenceses = '(';
-        gmMeta.pcParenceses++;
-        rTrace(&pmParent->pmChildren[iCon]);
-        *gmMeta.pcParenceses = ')';
-        gmMeta.pcParenceses++;
-    }
-}
-void rGenerate(unsigned int iAvailable, unsigned int iLeft, struct sParenthese *pmParent){
-    pmParent->iNumber = 0;
-    unsigned char *piChunks = (unsigned char *)malloc(sizeof(unsigned char) * iAvailable);
-    unsigned int iConN;
-    for(iConN = iAvailable ; iConN > 0 ; iConN--){
-        unsigned int iConChunk;
-        pmParent->iNumber = iConN;
-        pmParent->pmChildren = (struct sParenthese *)malloc(sizeof(struct sParenthese) * iConN);
-        if(iConN < iLeft){
-            unsigned int iNext = iAvailable - iConN;
-            for(iConChunk = 1 ; iConChunk < iConN ; iConChunk++){
-                piChunks[iConChunk] = 0;
-            }
-            piChunks[0] = iNext - 1;
-            do{
-                unsigned int iSum = 0 ;
-                piChunks[0]++;
-                for(iConChunk = 0 ; iConChunk < iConN - 1 ; iConChunk++){
-                    if(piChunks[iConChunk] > iNext){
-                        piChunks[iConChunk] = 0 ;
-                        piChunks[iConChunk + 1]++;
-                    }
-                    iSum += piChunks[iConChunk];
-                }
-                iSum += piChunks[iConChunk];
-                if(iSum == iNext){
-                    for(iConChunk = 0 ; iConChunk < iConN ; iConChunk++){
-                        rGenerate(piChunks[iConChunk], iLeft - iConN, &pmParent->pmChildren[iConChunk]);
-                    }
-                }
-            }while(piChunks[iConN - 1] != iNext);
-        }else{
-            struct sResult *pmResult = (struct sResult *)malloc(sizeof(struct sResult));
-            pmResult->pmNext = gmMeta.pmResults;
-            gmMeta.pmResults = pmResult;
-            pmResult->pcParentheses = (char *)malloc(sizeof(char) * (gmMeta.iNumber * 2 +1));
-            gmMeta.pcParenceses = pmResult->pcParentheses;
-            gmMeta.iResults++;
-            rTrace(&gmMeta.mParentheses);
-            *gmMeta.pcParenceses = 0;
-        }
-        free(pmParent->pmChildren);
-    }
+void rGenerate(unsigned int iNumber) {
+	struct sResult {
+		unsigned char iNumber;
+		char **ppcParentheeses;
+		struct sResult *pmNext;
+	};
+	struct sResult *pmResults = NULL;
+	unsigned int iConNumber;
+	for (iConNumber = iNumber; iConNumber > 0; iConNumber--) {
+		unsigned char iLeft = iNumber - iConNumber;
+		struct sChunk {
+			unsigned char iLeft;
+		};
+		struct sChunk *pmChunks = (struct sChunk *)malloc(sizeof(struct sChunk) * iConNumber);
+		unsigned int iConChunks;
+		for (iConChunks = 1; iConChunks < iConNumber; iConChunks++) {
+			pmChunks[iConChunks].iLeft = 0;
+		}
+		pmChunks[0].iLeft = iLeft - 1;
+		do {
+			unsigned int iSum = 0;
+			pmChunks[0].iLeft++;
+			for (iConChunks = 0; iConChunks < iConNumber - 1; iConChunks++) {
+				if (pmChunks[iConChunks].iLeft > iLeft) {
+					pmChunks[iConChunks].iLeft = 0;
+					pmChunks[iConChunks + 1].iLeft++;
+				}
+				iSum += pmChunks[iConChunks].iLeft;
+			}
+			iSum += pmChunks[iConChunks].iLeft;
+			if (iSum == iLeft) {
+				unsigned int iCombinations = 1;
+				for (iConChunks = 0; iConChunks < iConNumber; iConChunks++) {
+					if (gmMeta.pmCaches[pmChunks[iConChunks].iLeft].iCombinations == 0xFFFFFFFF){
+						rGenerate(pmChunks[iConChunks].iLeft);
+					}
+					iCombinations *= gmMeta.pmCaches[pmChunks[iConChunks].iLeft].iCombinations;
+				}
+				struct sResult *pmResult = (struct sResult *)malloc(sizeof(struct sResult));
+				pmResult->pmNext = pmResults;
+				pmResults = pmResult;
+				pmResult->iNumber = iCombinations;
+				pmResult->ppcParentheeses = (char **)malloc(sizeof(char *) * iCombinations);
+
+
+
+
+			}
+		} while (pmChunks[iConNumber - 1].iLeft != iLeft);
+	}
 }
 
 char ** generateParenthesis(int iNumber, int* piResults){
-    gmMeta.iResults = 0;
-    gmMeta.iNumber = iNumber;
-    gmMeta.pmResults = NULL;
-    rGenerate(3,3, &gmMeta.mParentheses);
-    char **ppcResults = (char **)malloc(sizeof(char *) * gmMeta.iResults);
-    unsigned iCon;
-    for(iCon = 0 ; iCon < gmMeta.iResults ; iCon++){
-        struct sResult *pmResult = gmMeta.pmResults;
-        ppcResults[iCon] = pmResult->pcParentheses;
-        gmMeta.pmResults = pmResult->pmNext;
-        free(pmResult);
-    }
-    *piResults = gmMeta.iResults;
-    return ppcResults;
+	static char *pcTemp[] = { "" };
+	unsigned int iCon;
+	gmMeta.pmCaches = (struct sCache *)malloc(sizeof(struct sCache) * iNumber);
+	gmMeta.pmCaches[0].iCombinations = 1;
+	gmMeta.pmCaches[0].ppcParentheses = pcTemp;
+	for (iCon = 1; iCon < iNumber; iCon++) {
+		gmMeta.pmCaches[iCon].iCombinations = 0xFFFFFFFF;
+	}
+	rGenerate(1);
 }
 
 int main(void){
