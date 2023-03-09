328281e10d28333bfec9be705e6d0fa63ab5953a
szwaymi <61036584+szwaymi@users.noreply.github.com>
2023-03-09 09:53:15 +0800

    [34] (Trying) 1. Fix some bugs found by test case 2. Remove the size = 1 detection.

--- 34/main.c
+++ 34/main.c
@@ -7,37 +7,52 @@ struct{
     int iTarget;
 }gmMeta;
 
-void rSearch(unsigned int iLeft, unsigned int iRight){
+void rSearch(int iLeft, int iRight){
+
+	//if (iLeft == iRight) { return; }
+	printf("[%d %d]", iLeft, iRight);
     int iMiddle = (iLeft + iRight) / 2;
+
     if(gmMeta.piSeries[iLeft] <= gmMeta.iTarget && gmMeta.piSeries[iMiddle] >= gmMeta.iTarget){
-        if(gmMeta.piSeries[iRight] == gmMeta.iTarget && iRight > gmMeta.iLocation[1]){
-            gmMeta.iLocation[1] = iRight;
+		unsigned char iAcc = 0;
+        if(gmMeta.piSeries[iMiddle] == gmMeta.iTarget && iMiddle > gmMeta.iLocation[1]){
+            gmMeta.iLocation[1] = iMiddle;
+			iAcc++;
         }
-        if(gmMeta.piSeries[iLeft] == gmMeta.iTarget && iLeft < gmMeta.iLocation[1]){
+        if(gmMeta.piSeries[iLeft] == gmMeta.iTarget && iLeft < gmMeta.iLocation[0]){
             gmMeta.iLocation[0] = iLeft;
+			iAcc++;
         }
-        rSearch(iLeft, iMiddle);
+		if (iAcc == 1) {
+			rSearch(iLeft, iMiddle);
+		}
     }
     if(gmMeta.piSeries[iMiddle+1] <= gmMeta.iTarget && gmMeta.piSeries[iRight] >= gmMeta.iTarget){
         if(gmMeta.piSeries[iRight] == gmMeta.iTarget && iRight > gmMeta.iLocation[1]){
             gmMeta.iLocation[1] = iRight;
         }
         if(gmMeta.piSeries[iMiddle + 1] == gmMeta.iTarget && iMiddle + 1 < gmMeta.iLocation[0]){
-            gmMeta.iLocation[0] = iLeft;
+            gmMeta.iLocation[0] = iMiddle + 1;
         }
         if(iMiddle + 1 < iRight){
             rSearch(iMiddle + 1, iRight);
         }
     }
 }
-
-int* searchRange(int* piSeries, int iSize, int iTarget, int* piResults){
-    gmMeta.iLocation[0] = iSize;
-    gmMeta.iLocation[1] = -1;
-    gmMeta.piSeries = piSeries;
-    gmMeta.iTarget = iTarget;
-    rSearch(0, iSize - 1);
-    return gmMeta.iLocation;
+int* searchRange(int* piSeries, int iSize, int iTarget, int* piResults) {
+	gmMeta.iLocation[0] = iSize;
+	gmMeta.iLocation[1] = -1;
+	if (iSize) {
+		gmMeta.piSeries = piSeries;
+		gmMeta.iTarget = iTarget;
+		rSearch(0, iSize - 1);
+	}
+	*piResults = 2;
+	int *piRange = (int *)malloc(sizeof(int) * 2);
+	if (gmMeta.iLocation[0] == iSize) { piRange[0] = -1; }
+	else { piRange[0] = gmMeta.iLocation[0]; }
+	piRange[1] = gmMeta.iLocation[1];
+	return piRange;
 }
 int main(void){
         
@@ -51,21 +66,30 @@ int main(void){
     };
     struct sExp{
         int iSize;
-        int *piLocation;
+        int *piRange;
     };
     struct sTest{
         unsigned int iNO;
         struct sInput mInput;
         struct sExp mExp;
     };
+	//#1
     M_TEST_INPUT(1, 5, 7, 7, 8, 8, 10);
     M_TEST_EXP(1, 3, 4);
-    //M_TEST_INPUT(2, 4, 5, 6, 7, 0, 1, 2);
-    //M_TEST_INPUT(3, 5, 1, 3);
+	//#2
+	M_TEST_INPUT(2, 5, 7, 7, 8, 8, 10);
+	M_TEST_EXP(2, -1, -1);
+	//#47
+	M_TEST_INPUT(47, 1);
+	M_TEST_EXP(47, 0, 0);
+	//#
+	M_TEST_INPUT(1024, 7, 7, 8);
+	M_TEST_EXP(1024, 2, 2);
     struct sTest mTest[]={
-        M_TEST_COLLECTION(1, 8),
-        //M_TEST_COLLECTION(2, 3, -1),
-        //M_TEST_COLLECTION(3, 1, 1),
+        //M_TEST_COLLECTION(1, 8),
+		//M_TEST_COLLECTION(2, 6),
+        M_TEST_COLLECTION(47, 1),
+        //M_TEST_COLLECTION(1024, 8),
     };
     
     unsigned int iLengthTest = sizeof(mTest) / sizeof(struct sTest);
@@ -75,7 +99,8 @@ int main(void){
         int iResultSize;
         int *piResult =
         searchRange(mTest[iConTest].mInput.piSeries, mTest[iConTest].mInput.iLength, mTest[iConTest].mInput.iTarget, &iResultSize);
-        printf("[%d %d] [%d %d]\n", mTest[iConTest].mExp.piLocation[0], mTest[iConTest].mExp.piLocation[1], piResult[0], piResult[1]);
+		printf("[%d %d]\n", piResult[0], piResult[1]);
+        //printf("[%d %d] [%d %d]\n", mTest[iConTest].mExp.piLocation[0], mTest[iConTest].mExp.piLocation[1], piResult[0], piResult[1]);
     }
     
     
