1f35803c01662cdafefc5cc6fee84219e2445b4c
szwaymi <szwaymi@hotmail.com>
2023-03-10 20:37:49 +0800

    [39] (Solved) First solution.

--- 39/main.c
+++ 39/main.c
@@ -1,6 +1,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <memory.h>
 
 void rTST_QuickSort(int *piValues, unsigned int iSize) {
 
@@ -37,80 +38,76 @@ void rTST_QuickSort(int *piValues, unsigned int iSize) {
 	}
 	return;
 }
+struct sResult{
+    int *piSeries;
+    int iLength;
+    struct sResult *pmNext;
+};
+static struct{
+    int *piSeries;
+    int iLength;
+    int iTarget;
+    int iNumber;
+    int *piResult;
+    struct sResult *pmResults;
+    int iResults;
+}gmMeta;
 
+void rMatch(int iNumber, int iSum, int iLoc){
+    if(iNumber == 0){
+        if(iSum == gmMeta.iTarget){
+            struct sResult *pmResult = (struct sResult *)malloc(sizeof(struct sResult));
+            pmResult->pmNext = gmMeta.pmResults;
+            gmMeta.pmResults = pmResult;
+            
+            pmResult->iLength = gmMeta.iNumber;
+            
+            pmResult->piSeries = (int *)malloc(sizeof(int) * pmResult->iLength);
+            
+            memcpy(pmResult->piSeries, gmMeta.piResult, sizeof(int) * pmResult->iLength);
+            gmMeta.iResults++;
+        }
+        return;
+    }
+    unsigned int iCon;
+    for(iCon = iLoc ; iCon < gmMeta.iLength ; iCon++){
+        gmMeta.piResult[iNumber - 1] = gmMeta.piSeries[iCon];
+        int iTemp = iSum + gmMeta.piSeries[iCon];
+        if(iTemp > gmMeta.iTarget){
+            break;
+        }
+        rMatch(iNumber - 1, iTemp, iCon);
+    }
+}
 int** combinationSum(int* piCandidates, int iLength, int iTarget, int* piResults, int** ppiLengthes){
 
+    
+    printf("\n");
 	rTST_QuickSort(piCandidates, iLength);
-	struct sResult {
-		int *piSeries;
-		unsigned int iNumber;
-		struct sResult *pmNext;
-	};
-	int iResults = 0;
-	struct sResult *pmResults = NULL;
-
-	unsigned int iConNumber;
-    unsigned int iNumber = iTarget / piCandidates[0];
-	
-	unsigned char *piAccs = (unsigned char *)malloc(sizeof(unsigned char) * (iNumber + 1));
-    for(iConNumber = 1 ; iConNumber <= iNumber ; iConNumber++){
-		unsigned int iConCombine;
-		//Initial for each
-		for (iConCombine = 0; iConCombine < iConNumber + 1; iConCombine++) {
-			piAccs[iConCombine] = 0;
-		}
-        do{
-			//Calculate Sum
-            int iSum = 0;
-            for(iConCombine = 0 ; iConCombine < iConNumber ; iConCombine++){
-				printf("%d ", piAccs[iConNumber - iConCombine - 1]);
-				if (piAccs[iConNumber - iConCombine - 1] == 4) {
-					printf("X");
-				}
-                iSum += piCandidates[piAccs[iConNumber - iConCombine - 1]];
-                if(iSum > iTarget){
-					piAccs[iConNumber - iConCombine - 1] = iLength;
-                    break;
-                }
-            }
-			//Produce result
-            if(iSum == iTarget && iConCombine == iConNumber){
-				struct sResult *pmResult = (struct sResult *)malloc(sizeof(struct sResult));
-				pmResult->pmNext = pmResults;
-				pmResults = pmResult;
-				pmResult->iNumber = iConNumber;
-				pmResult->piSeries = (int *)malloc(sizeof(int) * iConNumber);
-				unsigned int iConCombine;
-				for (iConCombine = 0; iConCombine < iConNumber; iConCombine++) {
-					pmResult->piSeries[iConCombine] = piCandidates[piAccs[iConCombine]];
-				}
-				iResults++;
-            }
-			//Acc to next
-            piAccs[0]++;
-            for(iConCombine = 0 ; iConCombine < iConNumber ; iConCombine++){
-                if(piAccs[iConCombine] >= iLength){
-                    piAccs[iConCombine] = 0;
-                    piAccs[iConCombine + 1]++;
-                }else{
-                    unsigned int iConBit;
-                    for(iConBit = 0 ; iConBit < iConCombine ; iConBit++){
-                        piAccs[iConBit] = piAccs[iConCombine];
-                    }
-                    break;
-                }
-            }
-        }while(piAccs[iConNumber] == 0);
-    }	
-	*piResults = iResults;
-	int **ppiResults = (int **)malloc(sizeof(int *) * iResults);
-	int *piLengthes = (int *)malloc(sizeof(int) * iResults);
+    
+    int iNumber = iTarget / piCandidates[0];
+    
+    gmMeta.piSeries = piCandidates;
+    gmMeta.iLength = iLength;
+    gmMeta.iTarget = iTarget;
+    gmMeta.piResult = (int *)malloc(sizeof(int) * iNumber);
+    gmMeta.pmResults = NULL;
+    gmMeta.iResults = 0;
+    unsigned int iCon;
+    for(iCon = 1 ; iCon <= iNumber ; iCon++){
+        gmMeta.iNumber = iCon;
+        rMatch(iCon, 0, 0);
+    }
+    
+	*piResults = gmMeta.iResults;
+    int **ppiResults = (int **)malloc(sizeof(int *) * gmMeta.iResults);
+    int *piLengthes = (int *)malloc(sizeof(int) * gmMeta.iResults);
 	unsigned int iConResults;
-	for (iConResults = 0; iConResults < iResults; iConResults++) {
-		struct sResult *pmResult = pmResults;
-		pmResults = pmResults->pmNext;
+	for (iConResults = 0; iConResults < gmMeta.iResults; iConResults++) {
+		struct sResult *pmResult = gmMeta.pmResults;
+		gmMeta.pmResults = gmMeta.pmResults->pmNext;
 		ppiResults[iConResults] = pmResult->piSeries;
-		piLengthes[iConResults] = pmResult->iNumber;
+		piLengthes[iConResults] = pmResult->iLength;
 		free(pmResult);
 	}
 	*ppiLengthes = piLengthes;
@@ -138,15 +135,19 @@ int main(void){
         struct sInput mInput;
 		struct sExp mExp;
     };
+    //#1
     M_TEST_INPUT(1, 2, 3, 6, 7);
 	M_TEST_EXP_SERIES(1, 2, 2, 3, 7);
 	M_TEST_EXP_LENGTH(1, 3, 1);
+    //#2
 	M_TEST_INPUT(2, 2, 3, 5);
 	M_TEST_EXP_SERIES(2, 2, 2, 2, 2, 2, 3, 3, 3, 5);
 	M_TEST_EXP_LENGTH(2, 4, 3, 2);
+    //#86
 	M_TEST_INPUT(86, 7, 3, 2);
 	M_TEST_EXP_SERIES(86, 1);
 	M_TEST_EXP_LENGTH(86, 1);
+    //#124
 	M_TEST_INPUT(124, 2, 22, 4, 17, 28, 13, 39, 27, 24, 37, 12, 30, 5, 23, 29, 8, 16, 34, 15, 36, 14, 10, 31);
 	M_TEST_EXP_SERIES(124, 1);
 	M_TEST_EXP_LENGTH(124, 1);
@@ -155,7 +156,7 @@ int main(void){
 		//M_TEST_COLLECTION(2, 8),
 		//M_TEST_COLLECTION(44, 11),
 		//M_TEST_COLLECTION(86, 18),
-		//M_TEST_COLLECTION(124, 30),
+		M_TEST_COLLECTION(124, 30),
     };
     unsigned int iLengthTest = sizeof(mTest) / sizeof(struct sTest);
     unsigned int iConTest;
