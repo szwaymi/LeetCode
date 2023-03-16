363f18500e19c90454df61885972a76104a29b7c
szwaymi <szwaymi@hotmail.com>
2023-03-16 23:21:12 +0800

    [53] (Trying)

--- 53/main.c
+++ 53/main.c
@@ -4,120 +4,48 @@
 #include <stdio.h>
 
 int maxSubArray(int* piSeries, int iLength) {
-
-	struct sResult{
-		int iLoc;
-		int iMax;
-		struct sResult *pmNext;
-	};
-
-	if (iLength == 1) {
-		return *piSeries;
-	}
-
-	struct sMax{
-		unsigned int iLength;
-		struct sResult mResult;
-	};
-
-	struct sMax mLeft;
-	struct sMax mRight;
-
-	mLeft.mResult.iLoc = 0;
-	mLeft.mResult.pmNext = NULL;
-	mLeft.iLength = 0;
-	mRight.mResult.iLoc = iLength - 1;
-	mRight.mResult.pmNext = NULL;
-	mRight.iLength = 0;
-
-	int iCon;
-	int iMax;
-	int iSum;
-
-	iSum = 0;
-	iMax = 0x80000000;
-	for (iCon = 0; iCon < iLength; iCon++) {
-		iSum += piSeries[iCon];
-		if (iSum >= iMax) {
-			iMax = iSum;
-			mRight.mResult.iLoc = iCon;
-			struct sResult *pmResult = (struct sResult *)malloc(sizeof(struct sResult));
-			pmResult->iLoc = iCon;
-			pmResult->iMax = iSum;
-			pmResult->pmNext = mRight.mResult.pmNext;
-			mRight.mResult.pmNext = pmResult;
-			mRight.iLength++;
-		}
-	}
-	mRight.mResult.iMax = iMax;
-
-	iSum = 0;
-	iMax = 0x80000000;
-	for (iCon = 0; iCon < iLength; iCon++) {
-		iSum += piSeries[iLength - 1 - iCon];
-		if (iSum >= iMax) {
-			iMax = iSum;
-			mLeft.mResult.iLoc = iLength - 1 - iCon;
-			struct sResult *pmResult = (struct sResult *)malloc(sizeof(struct sResult));
-			pmResult->iLoc = iLength - 1 - iCon;
-			pmResult->iMax = iSum;
-			pmResult->pmNext = mLeft.mResult.pmNext;
-			mLeft.mResult.pmNext = pmResult;
-			mLeft.iLength++;
-		}
-	}
-	mLeft.mResult.iMax = iMax;
-
-	printf("%d %d\n", mLeft.mResult.iLoc, mRight.mResult.iLoc);
-
-	if (mRight.mResult.iLoc < mLeft.mResult.iLoc && mLeft.iLength == 1 && mRight.iLength == 1){
-		iSum = 0x80000000;
-		for (iCon = 0; iCon < iLength; iCon++) {
-			if (piSeries[iCon] > iSum) {
-				iSum = piSeries[iCon];
-			}
-		}
-		
-	}
-	else {
-
-		iSum = 0x80000000;
-		while (mLeft.mResult.pmNext) {
-			struct sResult *pmLeft = mLeft.mResult.pmNext;
-			mLeft.mResult.pmNext = pmLeft->pmNext;
-			if (pmLeft->iMax == mLeft.mResult.iMax) {
-				struct sResult *pmRight = NULL;
-				struct sResult *pmTemp = mRight.mResult.pmNext;
-				int iDiff = 0x7FFFFFFF;
-				while (pmTemp) {
-					if (pmTemp->iMax == mRight.mResult.iMax) {
-						int iTemp = abs(pmTemp->iLoc - pmLeft->iLoc);
-						if (iTemp < iDiff) {
-							iDiff = iTemp;
-							pmRight = pmTemp;
-						}
-					}
-					pmTemp = pmTemp->pmNext;
-				}
-				int iTemp = 0;
-				for (iCon = pmLeft->iLoc; iCon <= pmRight->iLoc; iCon++) {
-					iTemp += piSeries[iCon];
-				}
-				if (iTemp > iSum) {
-					iSum = iTemp;
-				}
-			}
-			free(pmLeft);
-		}
-
-		while (mRight.mResult.pmNext) {
-			struct sResult *pmResult = mRight.mResult.pmNext;
-			mRight.mResult.pmNext = pmResult->pmNext;
-			free(pmResult);
-		}
-	
-	}
-	return iSum;
+    int *piLeft = (int *)malloc(sizeof(int) * iLength);
+    int *piRight = (int *)malloc(sizeof(int) * iLength);
+    int iSum;
+    int iTotal;
+    unsigned int iCon;
+    struct{
+        int iMin;
+        unsigned int iLoc;
+    }mLeft;
+    
+    mLeft.iMin = 0x7FFFFFFF;
+    iSum = 0;
+    piLeft[0] = 0;
+    for(iCon = 1 ; iCon < iLength ; iCon++){
+        iSum += piSeries[iCon - 1];
+        piLeft[iCon] = iSum;
+    }
+    iTotal = iSum + piSeries[iCon - 1];
+    
+    iSum = 0;
+    piRight[0] = 0;
+    for(iCon = 0 ; iCon < iLength - 1 ; iCon++){
+        iSum += piSeries[iLength - 1 - iCon];
+        piRight[iCon + 1] = iSum;
+    }
+    
+    int iLast = 1;
+    int iMin = 0x7FFFFFFF;
+    for(iCon = 0 ; iCon < iLength ; iCon++){
+        unsigned int iConT;
+        if(piRight[iCon] >= iLast){
+            continue;;
+        }
+        for(iConT = 0 ; iConT < iLength - iCon ; iConT++){
+            int iTemp = piRight[iCon] + piLeft[iConT];
+            if(iTemp < iMin){
+                iMin = iTemp;
+            }
+        }
+        iLast = piRight[iCon];
+    }
+    return iTotal - iMin;
 }
 
 int main(void){
@@ -154,12 +82,11 @@ int main(void){
 	M_TEST_INPUT(184, 1, 2, -1, -2, 2, 1, -2, 1);
 	M_TEST_INPUT(1024, -10, -1, -1, 1, 2, 3);
 	M_TEST_INPUT(1025, -100, -2, 1, 1, 1, 1,-300);
+    M_TEST_INPUT(1026, -1, -1, -1, -1, -1, -1, -1);
 	struct sTest mTest[] = {
-		M_TEST_COLLECTION(178, 0),
-		/*
+        M_TEST_COLLECTION(1026, -1),
 		M_TEST_COLLECTION(1, 6),
 		M_TEST_COLLECTION(2, 1),
-		M_TEST_COLLECTION(3, 23),
 		M_TEST_COLLECTION(3, 23),
 		M_TEST_COLLECTION(11, 1),
 		M_TEST_COLLECTION(13, 0),
@@ -172,7 +99,6 @@ int main(void){
 		M_TEST_COLLECTION(184, 3),
 		M_TEST_COLLECTION(1024, 6),
 		M_TEST_COLLECTION(1025, 4),
-		*/
     };
     unsigned int iLengthTest = sizeof(mTest) / sizeof(struct sTest);
     unsigned int iConTest;
