1d271c323a344245955e08637738723fd88f6b64
szwaymi <61036584+szwaymi@users.noreply.github.com>
2023-03-20 11:47:12 +0800

    [89] (Solved) First solution

--- 89/main.c
+++ 89/main.c
@@ -3,38 +3,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define DBPRINT printf
+
 void rProduce(int *piSeries, unsigned int iNumber){
     if(iNumber == 1){
         piSeries[0] = 0;
         piSeries[1] = 1;
         return;
     }
-    unsigned int iHalf = iNumber / 2;
-    rProduce(piSeries, iHalf);
+    unsigned int iHalf = (1 << iNumber) / 2;
+    rProduce(piSeries, iNumber - 1);
     unsigned int iCon;
+	int iMask = 1 << (iNumber - 1);
     for(iCon = 0 ; iCon < iHalf ; iCon++){
-        
+		piSeries[iHalf + iCon] = iMask | piSeries[iHalf - iCon - 1];
     }
-    
 }
 
-int* grayCode(int iNumber, int* *piLength){
+int* grayCode(int iNumber, int* piLength){
+	DBPRINT("\n");
     unsigned int iLength = 1 << iNumber;
+	DBPRINT("Length = %d\n", iLength);
     int *piSeries = (int *)malloc(sizeof(int) * iLength);
     
     rProduce(piSeries, iNumber);
+	*piLength = iLength;
+	return piSeries;
 }
+
 
 int main(void){
     //Test Data
     //  Macro
 #define M_TEST_INPUT
-#define M_TEST_EXP
-#define M_TEST_COLLECTION
+#define M_TEST_EXP(N, ...) int iExp_##N[] = { __VA_ARGS__ }
+#define M_TEST_COLLECTION(N, S) {N, {S}, {iExp_##N, sizeof(iExp_##N) / sizeof(int)}}
     //  Structure
     struct sInput{
+		int iNumber;
     };
     struct sExp{
+		int *piSeries;
+		int iLength;
     };
     struct sTest{
         unsigned int iNO;
@@ -42,7 +52,11 @@ int main(void){
         struct sExp mExp;
     };
     //  Data
+	M_TEST_EXP(1, 0, 1, 3, 2);
+	M_TEST_EXP(3, 0, 1, 3, 2, 6, 7, 5, 4);
     struct sTest mTest[]={
+		//M_TEST_COLLECTION(1, 2),
+		M_TEST_COLLECTION(3, 3)
     };
     unsigned int iLengthTest = sizeof(mTest) / sizeof(struct sTest);
     unsigned int iConTest;
@@ -50,10 +64,23 @@ int main(void){
         printf("Test Case [%d], ", mTest[iConTest].iNO);
         //Expectaion
         printf("Expecation = ");
+		printf("[");
+		for (unsigned int iConExp = 0; iConExp < mTest[iConTest].mExp.iLength; iConExp++) {
+			printf("%d ", mTest[iConTest].mExp.piSeries[iConExp]);
+		}
+		printf("], ");
         //Result
         printf("Result = ");
+		int iLength;
+		int *piResult;
+		piResult = grayCode(mTest[iConTest].mInput.iNumber, &iLength);
+		printf("[");
+		for (unsigned int iConResult = 0; iConResult < iLength; iConResult++) {
+			printf("%d ", piResult[iConResult]);
+		}
+		printf("], ");
         //Comparison
-        int iTest;
+        int iTest = 1;
         if(iTest){
             printf("[PASS]");
         }else{
