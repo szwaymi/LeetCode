af7c6f5c202f49fcb17860df67e43f190d3e9dc5
szwaymi <szwaymi@hotmail.com>
2023-03-25 07:14:40 +0800

    [65] (Creating) Build the test platform.

--- 65/main.c
+++ 65/main.c
@@ -1,4 +1,12 @@
 
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define bool int
+#define true 1
+#define false 0
+
 bool isNumber(char * pcNumber){
     unsigned char iState = 0;
 
@@ -61,3 +69,63 @@ bool isNumber(char * pcNumber){
     }
     return true;
 }
+
+int main(void){
+    //Test Data
+    //  Macro
+#define M_TEST_INPUT
+#define M_TEST_EXP
+#define M_TEST_COLLECTION(N, S, R) {N, {S}, {R}}
+    //  Structure
+    struct sInput{
+        char *pcNumber;
+    };
+    struct sExp{
+        bool fResult;
+    };
+    struct sTest{
+        unsigned int iNO;
+        struct sInput mInput;
+        struct sExp mExp;
+    };
+    //  Data
+    struct sTest mTest[]={
+        M_TEST_COLLECTION(1, "0", true),
+    };
+    unsigned int iLengthTest = sizeof(mTest) / sizeof(struct sTest);
+    unsigned int iConTest;
+    for(iConTest = 0 ; iConTest < iLengthTest ; iConTest++){
+        printf("Test Case [%d], ", mTest[iConTest].iNO);
+        //Expectaion
+        printf("Expecation = ");
+        if(mTest[iConTest].mExp.fResult){printf("TRUE");}
+        else{printf("FALSE");}
+        printf(" ");
+        //Result
+        printf("Result = ");
+        bool fResult =
+        isNumber(mTest[iConTest].mInput.pcNumber);
+        if(fResult){printf("TRUE");}
+        else{printf("FALSE");}
+        printf(" ");
+        //Comparison
+        int iTest = mTest[iConTest].mExp.fResult == fResult;
+        if(iTest){
+            printf("[PASS]");
+        }else{
+            printf("[FAIL]");
+        }
+        printf("\n");
+        if(!iTest){
+            break;
+        }
+    }
+    printf("All Test Case : ");
+    if(iConTest < iLengthTest){
+        printf("[FAIL]");
+    }else{
+        printf("[PASS]");
+    }
+    printf("\n");
+}
+
