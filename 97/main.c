ac94c01eaa2250076c422a6197e8d2842702ce87
szwaymi <szwaymi@hotmail.com>
2023-04-06 22:48:15 +0800

    [97] (Trying) IR #2

--- 97/main.c
+++ 97/main.c
@@ -26,9 +26,18 @@ bool isInterleave(char * pcSrcA, char * pcSrcB, char * pcDes){
                 return false;
             }
             if(pcTail == NULL){
+                int iOffset = (int)(pcSearch - pcHead);
+                pcDes -= (iOffset + 1);
+                pcSearch = pcCheck - iOffset;
+                pcCheck = pcHead - 1;
+                
+                /*
+                
                 char *pcTemp = pcCheck;
                 pcCheck = pcSearch;
                 pcSearch = pcTemp;
+                */
+                
             }else{
                 int iOffset = (int)(pcSearch - pcHead);
                 pcCheck = pcSearch;
@@ -68,14 +77,15 @@ int main(void){
     };
     //  Data
     struct sTest mTest[]={
+        //M_TEST_COLLECTION(1024, true, "abc", "abcxyz", "abcxyzabc"),
         //M_TEST_COLLECTION(106, false, "abababababababababababababababababababababababababababababababababababababababababababababababababbb", "babababababababababababababababababababababababababababababababababababababababababababababababaaaba", "abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababbb"),
         //M_TEST_COLLECTION(105, true, "aacaac", "aacaaeaac", "aacaacaaeaacaac"),
         //M_TEST_COLLECTION(104, true, "abababababababababababababababababababababababababababababababababababababababababababababababababbb", "abababababababababababababababababababababababababababababababababababababababababababababababababab", "abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababbb"),
         //M_TEST_COLLECTION(100, false, "bbbbbabbbbabaababaaaabbababbaaabbabbaaabaaaaababbbababbbbbabbbbababbabaabababbbaabababababbbaaababaa", "babaaaabbababbbabbbbaabaabbaabbbbaabaaabaababaaaabaaabbaaabaaaabaabaabbbbbbbbbbbabaaabbababbabbabaab", "babbbabbbaaabbababbbbababaabbabaabaaabbbbabbbaaabbbaaaaabbbbaabbaaabababbaaaaaabababbababaababbababbbababbbbaaaabaabbabbaaaaabbabbaaaabbbaabaaabaababaababbaaabbbbbabbbbaabbabaabbbbabaaabbababbabbabbab"),
         //M_TEST_COLLECTION(87, true, "aa", "ab", "aaba"),
         M_TEST_COLLECTION(2, false, "aabcc", "dbbca", "aadbbbaccc"),
-        //M_TEST_COLLECTION(1, true, "aabcc", "dbbca", "aadbbcbcac"),
-		//M_TEST_COLLECTION(1024, true, "abc", "abcxyz", "abcxyzabc"),
+        M_TEST_COLLECTION(1, true, "aabcc", "dbbca", "aadbbcbcac"),
+		
     };
     unsigned int iLengthTest = sizeof(mTest) / sizeof(struct sTest);
     unsigned int iConTest;
